3734eafeba4b5ea07d38f47e4bbfdb8fce76f6ff
thieukhacthanh27 <117759050+thieukhacthanh27@users.noreply.github.com>
2026-09-15 01:57:41 +0700

    lab.py

--- lab.py
+++ lab.py
@@ -7,7 +7,7 @@ from datetime import datetime
 # CẤU HÌNH CƠ BẢN
 # ==========================================
 st.set_page_config(page_title="Hệ thống Quản lý Mẫu - Lab GC", layout="wide")
-DATA_FILE = "https://docs.google.com/spreadsheets/d/1F2wFnxboWTFWDMGUuBDRGB901a5EKgvazHxkCgBjjRU/edit?usp=sharing"
+DATA_FILE = "https://docs.google.com/spreadsheets/d/1F2wFnxboWTFWDMGUuBDRGB901a5EKgvazHxkCgBjjRU/edit?gid=0#gid=0"
 
 STATUSES = [
     "🔴 1. Chờ xử lý", "🟠 2. Đang xử lý mẫu", "🟡 3. Chờ chạy máy",
